cb028728a77599565d81c2a2751a0c595bcce7d9
caosdan@gmail.com <caosdan@gmail.com>
2020-06-03 21:36:24 -0300

    Cadastro de ponto de coleta - implementação da rota /points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -17,8 +17,55 @@ routes.get("/items", async (request, response) => {
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
-
   return response.json(serializedItems);
+});
+
+routes.post("/points", async (request, response) => {
+  /*  Utilizando desestruturação para atribuir cada valor vindo em request.body 
+  para uma constante específica. É o mesmo que atribuir do modo abaixo:
+
+  const name = request.body.name
+  const email = request.body.email
+  ...
+  const uf = request.body.uf
+
+  */
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  /*Utilizando short sintax: quando o nome da variável é igual ao nome do objeto,
+  podemos omitir o valor*/
+  const insertedIds = await trx("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      point_id: insertedIds[0],
+      item_id,
+    };
+  });
+
+  await trx("point_items").insert(pointItems);
+
+  return response.json({ sucess: true });
 });
 
 export default routes;
