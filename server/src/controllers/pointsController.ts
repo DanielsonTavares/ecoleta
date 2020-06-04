b06ca11bbae52902615a4451f69375eb29bdb397
caosdan@gmail.com <caosdan@gmail.com>
2020-06-03 23:33:56 -0300

    Listar pontos (filtro por estado/cidade/itens)

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -2,6 +2,25 @@ import { Request, Response } from "express";
 import knex from "../database/connection";
 
 class PointsController {
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+    console.log(city, uf, items);
+
+    const parsedItems = String(items)
+      .split(",")
+      .map((item) => Number(item.trim()));
+
+    const points = await knex("points")
+      .join("point_items", "point_id", "=", "point_items.point_id")
+      .whereIn("point_items.item_id", parsedItems)
+      .where("city", String(city))
+      .where("uf", String(uf))
+      .distinct()
+      .select("points.*");
+
+    return response.json(points);
+  }
+
   async show(request: Request, response: Response) {
     const { id } = request.params; //Desestruturação: equivale a id = request.params.id;
     const point = await knex("points").where("id", id).first(); //o .first() está sendo utilizado para garantir que não seja retornado um array
