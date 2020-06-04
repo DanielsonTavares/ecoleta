536d3b0153532dfe997ac66e1f758238a3b9243a
caosdan@gmail.com <caosdan@gmail.com>
2020-06-03 22:33:36 -0300

    Criação da itemsController.js

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,27 +1,23 @@
 import express from "express";
-import knex from "./database/connection";
 import PointsController from "./controllers/pointsController";
+import ItemsController from "./controllers/itemsController";
 
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 routes.get("/", (request, response) => {
   return response.json(["danielson", "vanessa", "isadora", "dan"]);
 });
 
-routes.get("/items", async (request, response) => {
-  const items = await knex("items").select("*");
-
-  const serializedItems = items.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-  return response.json(serializedItems);
-});
-
+/* Padrão de métodos para as controllers:
+  index para listagens;
+  show para um único item;
+  create para criar um item;
+  update para atualizar um item;
+  delete para remover um item;
+ */
+routes.get("/items", itemsController.index);
 routes.post("/points", pointsController.create);
 
 export default routes;
