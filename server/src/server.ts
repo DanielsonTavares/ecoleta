f537fed05f18944da0f7035653a11be994c11893
caosdan@gmail.com <caosdan@gmail.com>
2020-06-03 23:42:19 -0300

    Adicionando CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express, { response } from "express";
 import path from "path";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 
+//Define quais endereços terão acesso a nossa API.
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
