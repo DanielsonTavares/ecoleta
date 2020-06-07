5cb5286ccd71c26dc3fb3651729c77d646d23dbf
danielson <caosdan@gmail.com>
2020-06-07 06:26:27 -0300

    Configurando a navegação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,7 @@ import { StatusBar, Image } from "react-native";
 import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
 import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
 
-import Home from "./src/pages/home";
+import Routes from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +26,7 @@ export default function App() {
         translucent
       />
 
-      <Home />
+      <Routes />
     </>
   );
 }
